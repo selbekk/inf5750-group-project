6843a67205dabd07c306e211c544bcf69de3c3a9
selbekk <kristofer.selbekk@bekk.no>
2014-10-29 09:24:13 +0100

    Refactor router into its own file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,1 @@
 var app = angular.module('apiViewer', ['ngRoute']);
-
-app.config(function($routeProvider) {
-        $routeProvider
-            .when('/list', {
-                controller: 'ListController',
-                templateUrl: 'views/list.html'
-            })
-            .when('/single/:id', {
-                controller: 'SingleController',
-                templateUrl:'views/single.html'
-            })
-            .otherwise({redirectTo: '/list'});
-});
